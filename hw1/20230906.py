c9cce10f0943813a774a2f3a17c1231403aeacf3
cindyfan000 <81600669+cindyfan000@users.noreply.github.com>
2023-09-06 23:03:52 +0800

    Add files via upload

--- hw1/20230906.py
+++ hw1/20230906.py
@@ -1,3 +1,16 @@
+#%% 數學運算
+a = 100
+b = 3
+print(f"{a + b = }")
+print(f"{a - b = }")
+print(f"{a * b = }")
+print(f"{a / b = }") # 結果是浮點數
+print(f"{a // b = }") # 結果是整數
+print(f"{a % b = }") # 兩數相除的餘數
+print(f"{a ** b = }") # 指數
+print()
+
+#%% IPO
 # 請求用戶輸入身高和體重
 height = float(input("糗輸入你的身高(公尺):"))
 weight = float(input("糗輸入你的體重(公斤):"))
@@ -7,3 +20,4 @@ BMI = weight/(height**2)
 
 # 顯示計算得到的BMI值
 print(f"您的BMI是:{BMI:.2f}")
+
